bd611c51bb65286e3773597672beb6aa01670b3d
petersercombe <peter.sercombe@bcc.net.au>
2022-03-04 12:32:15 +1000

    Friday 4th March

--- pizza.py
+++ pizza.py
@@ -3,23 +3,48 @@ from myUtils import *
 
 print("Welcome to Pete's Uhh Emporium")
 
-# Choose a size
-sizeOptions = ["Small", "Medium", "Large", "X-Large"]
-sizeChoice = choice("Size", sizeOptions)
+sizeChoice = []
+toppingChoice = []
 
-# Choose a topping
-toppingOptions = ["Meatlovers", "Hawaiian", "Triple Cheese and Bacon"]
-toppingChoice = choice("Topping", toppingOptions)
+while True:
+    # Choose a size
+    sizeOptions = ["Small", "Medium", "Large", "X-Large"]
+    sizeChoice.append(choice("Size", sizeOptions))
 
-# Choose a drink
-drinkOptions = ["Coca Cola", "Fanta", "Bundaberg Ginger Beer", "No Drink"]
-drinkChoice = choice("Drink", drinkOptions)
+    # Choose a topping
+    toppingOptions = ["Meatlovers", "Hawaiian", "Triple Cheese and Bacon"]
+    toppingChoice.append(choice("Topping", toppingOptions))
+
+    if input("Would you like another pizza? y/n: ").lower() == "y":
+        continue
+    else:
+        break
+
+
+drinkChoice = []
+while True:
+    # Choose a drink
+    drinkOptions = ["No Drink", "Coca Cola", "Fanta", "Bundaberg Ginger Beer"]
+    drinkChoice.append(choice("Drink", drinkOptions))
+
+    if input("Would you like another drink? y/n: ").lower() == "y":
+        continue
+    else:
+        break
+
 
 # Choose a side
-sideOptions = ["Garlic Bread", "Hot Fudge", "Churros", "Mini Dutch Pancakes"]
+sideOptions = ["No Side", "Garlic Bread", "Hot Fudge", "Churros", "Mini Dutch Pancakes"]
 sideChoice = choice("Side", sideOptions)
 
+
 print("You have ordered:")
-print(sizeOptions[sizeChoice], toppingOptions[toppingChoice])
-print(drinkOptions[drinkChoice])
+
+for choice in sizeChoice:
+    print(sizeOptions[sizeChoice[sizeChoice.index(choice)]],
+          toppingOptions[toppingChoice[sizeChoice.index(choice)]])
+
+for choice in drinkChoice:
+    print(drinkOptions[drinkChoice[drinkChoice.index(choice)]])
+
 print(sideOptions[sideChoice])
