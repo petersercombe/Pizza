d4509021bf9b05a454c70673f846a430ce34471e
petersercombe <peter.sercombe@bcc.net.au>
2022-03-17 14:52:56 +1000

    Thursday 17th March

--- pizza.py
+++ pizza.py
@@ -15,10 +15,18 @@ while True:
     price += sizeOptions[userEntry][1]
 
     # Choose a topping
-    toppingOptions = ["Meatlovers", "Hawaiian", "Triple Cheese and Bacon"]
-    toppingChoice.append(choice("Topping", toppingOptions))
+    toppingOptions = [("Meatlovers", 0.00), ("Hawaiian", 0.00), ("Triple Cheese and Bacon", 1.00)]
+    userEntry = choice("Topping", toppingOptions)
+    toppingChoice.append(userEntry)
+    price += toppingOptions[userEntry][1]
 
-    if input("Would you like another pizza? y/n: ").lower() == "y":
+    while True:
+        another = input("Would you like another pizza? y/n: ").lower()
+        if another == "n" or another == "y":
+            break
+        else:
+            continue
+    if another == "y":
         continue
     else:
         break
@@ -27,29 +35,49 @@ while True:
 drinkChoice = []
 while True:
     # Choose a drink
-    drinkOptions = ["No Drink", "Coca Cola", "Fanta", "Bundaberg Ginger Beer"]
-    drinkChoice.append(choice("Drink", drinkOptions))
+    drinkOptions = [("No Drink", 0.00),
+                    ("Coca Cola", 2.50),
+                    ("Fanta", 2.50),
+                    ("Sprite", 2.50),
+                    ("Bundaberg Ginger Beer", 3.50)
+                    ]
+    userEntry = choice("Drink", drinkOptions)
+    drinkChoice.append(userEntry)
+    price += drinkOptions[userEntry][1]
 
     if input("Would you like another drink? y/n: ").lower() == "y":
         continue
     else:
         break
 
+sideChoice = []
+while True:
+    # Choose a side
+    sideOptions = [("No Side", 0.00),
+                   ("Garlic Bread", 2.00),
+                   ("Hot Fudge", 4.00),
+                   ("Churros", 3.00),
+                   ("Mini Dutch Pancakes", 4.00)]
+    userEntry = choice("Side", sideOptions)
+    sideChoice.append(userEntry)
+    price += sideOptions[userEntry][1]
 
-# Choose a side
-sideOptions = ["No Side", "Garlic Bread", "Hot Fudge", "Churros", "Mini Dutch Pancakes"]
-sideChoice = choice("Side", sideOptions)
+    if input("Would you like another side? y/n: ").lower() == "y":
+        continue
+    else:
+        break
 
 
 print("You have ordered:")
 
 for choice in sizeChoice:
-    print(sizeOptions[sizeChoice[sizeChoice.index(choice)]],
-          toppingOptions[toppingChoice[sizeChoice.index(choice)]])
+    print(sizeOptions[sizeChoice[sizeChoice.index(choice)]][0],
+          toppingOptions[toppingChoice[sizeChoice.index(choice)]][0])
 
 for choice in drinkChoice:
-    print(drinkOptions[drinkChoice[drinkChoice.index(choice)]])
+    print(drinkOptions[drinkChoice[drinkChoice.index(choice)]][0])
 
-print(sideOptions[sideChoice])
+for choice in sideChoice:
+    print(sideOptions[sideChoice[sideChoice.index(choice)]][0])
 
-# print the price out later.
+print("For a total of ${:.2f}".format(price))
